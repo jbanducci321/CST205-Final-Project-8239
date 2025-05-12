4bca787a858bac54326178b5132038aaa764c964
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2025-05-12 14:20:40 -0700

    Update and rename questions.py to get_images.py

--- questions.py
+++
@@ -1,9 +0,0 @@
-questions = [
-    {
-        "Excited" : ["What are you excited about?", ""],
-        "Happy" : ["What made you happy?", ""],
-        "Neutral" : ["What made you neutral?", ""],
-        "Upset" : ["What made you upset?", ""],
-        "Angry" : ["What made you angry?", ""]
-    }
-]
