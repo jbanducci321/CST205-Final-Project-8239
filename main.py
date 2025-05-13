1a4e7f8f10ac600e0610d6526d1957b3158f380a
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2025-05-13 15:43:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
 import sys
 from PySide6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QLineEdit, 
-                                QHBoxLayout, QVBoxLayout, QDialog, QTextBrowser, QComboBox)
-from PySide6.QtGui import QPixmap, QImage
+                                QHBoxLayout, QVBoxLayout, QDialog, QTextBrowser, 
+                                QMainWindow, QComboBox, QStackedWidget)
+# from PySide6.QtGui import QPixmap, QImage
 from PySide6.QtCore import Slot, Qt
 from __feature__ import snake_case, true_property
+from first_page import Page1
+from second_page import Page2
 
-app = QApplication([])
+# class AnotherWindow(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         layout = QVBoxLayout()
+#         self.label = QLabel("Another Window")
+#         layout.add_widget(self.label)
+#         self.set_layout(layout)
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        vbox = QVBoxLayout()
-        main_menu = QLabel("Welcome to the Main Menu")
-        my_btn = QPushButton("Enter")
-        self.my_label = QLabel()
-        my_btn.clicked.connect(self.on_click)
 
-        vbox.add_widget(main_menu)
-        vbox.add_widget(my_btn)
-        vbox.add_widget(self.my_label)
-        self.set_layout(vbox)
-        self.show()
+        self.stacked_widget = QStackedWidget()
 
-    @Slot()
-    def on_click(self):
-        self.my_label.text = "Button Pressed!"
+        # Create some pages
+        self.page1 = QLabel("Page 1")
+        self.page2 = QLabel("Page 2")
+
+        # Add pages to the stacked widget
+        self.stacked_widget.add_widget(self.page1)
+        self.stacked_widget.add_widget(self.page2)
+
+        # Create buttons to switch pages
+        self.button1 = QPushButton("Go to Page 1")
+        self.button1.clicked.connect(self.show_page1)
+        self.button2 = QPushButton("Go to Page 2")
+        self.button2.clicked.connect(self.show_page2)
+
+        layout = QVBoxLayout()
+        layout.add_widget(self.button1)
+        layout.add_widget(self.button2)
+        layout.add_widget(self.stacked_widget)
+        self.set_layout(layout)
+
+        # main_menu = QLabel("Welcome to the Main Page")
+        # enter_btn = QPushButton("Enter")
+        # self.my_label = QLabel()
+        # enter_btn.clicked.connect(self.on_click)
+
+        # vbox = QVBoxLayout()
+        # vbox.add_widget(main_menu)
+        # vbox.add_widget(enter_btn)
+        # vbox.add_widget(self.my_label)
+        # self.set_layout(vbox)
+
+        
+
+    def show_page1(self):
+        self.stacked_widget.set_current_index = 0
+        page_1 = Page1()
+        page_1.show()
+        #sys.exit(app.exec())   
+
+    def show_page2(self):
+        self.stacked_widget.set_current_index = 1
+        page_2 = Page2()
+        page_2.show()
+        #sys.exit(app.exec())   
+
+    # @Slot()
+    # def on_click(self):
+    #     self.my_label.text = "Success!"
 
 
-main_window = MainWindow()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    app.exec() 
